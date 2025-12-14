ebd630dee2ecafc4eef7b36a106c3fe7038a4190
lawi22 <guerpillon.louis@gmail.com>
2025-12-14 21:57:05 +0100

    day6 p1

---
+++ 6/Day6.java
@@ -0,0 +1,80 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Day6 {
+
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("6/input.txt"));
+
+        execute(lines);
+    }
+
+    public static void execute(List<String> lines) {
+
+        // map Key= index, Value = numbers to operate on
+        Map<Integer, List<Integer>> hm = new HashMap<>();
+        String[] operatorList = new String[lines.get(0).length() + 1];
+        System.out.println(operatorList.length);
+        int indexCount = 0;
+        Long total = 0l;
+
+        for (String l : lines) {
+            String[] chops = l.trim().split("\\s+");
+            System.out.println("NEW CHOPS");
+            for (String s : chops) {
+                System.out.println(s);
+            }
+
+            for (int i = 0; i < chops.length; i++) {
+                if (hm.containsKey(i)) {
+                    if (isAnOperator(chops[i])) {
+                        operatorList[i] = chops[i];
+                    } else {
+                        hm.get(i).add(Integer.valueOf(chops[i]));
+                    }
+                    System.out.println("added " + chops[i] + "at index: " + i);
+
+                } else {
+                    List<Integer> column = new ArrayList<>();
+                    column.add(Integer.valueOf(chops[i]));
+                    hm.put(i, column);
+                }
+
+            }
+
+            //System.out.println(l);
+        }
+        for (Map.Entry<Integer, List<Integer>> entry : hm.entrySet()) {
+
+            if (operatorList[entry.getKey()].equals("*")) {
+                Long curr = 1l;
+                for (Integer i : entry.getValue()) {
+                    curr *= i;
+                }
+                total += curr;
+            } else if (operatorList[entry.getKey()].equals("+")) {
+                Long curr = 0l;
+                for (Integer i : entry.getValue()) {
+                    curr += i;
+                }
+                total += curr;
+            }
+        }
+        System.out.println("Total: " + total);
+    }
+
+    private static boolean isAnOperator(String s) {
+        boolean b = false;
+        if (s.equals("*") || s.equals("+")) {
+            b = true;
+        }
+        return b;
+    }
+
+}
